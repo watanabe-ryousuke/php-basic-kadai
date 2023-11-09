b20a7b0c39674b35a87a7d251d5ae21636ce4a7a
watanabe-ryousuke <146714563+watanabe-ryousuke@users.noreply.github.com>
2023-11-09 17:26:12 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編</title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+         $array = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+         print_r($array);
+         ?>
+     </p>
+ </body>
+ 
+ </html>
